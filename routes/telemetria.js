8ebe088b463e90f663fb91aa7374004f2954d301
unknown <diegomanuelcaca@gmail.com>
2025-04-03 16:08:04 -0500

    98 auth

--- routes/telemetria.js
+++ routes/telemetria.js
@@ -3,7 +3,7 @@
 import { Router } from 'express';
 import telemetriaController from '../controllers/telemetria.js';
 import Telemetria from "../models/telemetria.js";
-import validarApiKey from "../middlewares/validar_api.js";
+// import validarApiKey from "../middlewares/validar_api.js";
 
 const router = Router();
 
@@ -54,7 +54,17 @@ router.post("/telemetry/webhook", telemetriaController.receiveTelemetry);
 
 
 // Ruta para obtener histórico
-router.get("/historico", validarApiKey, async (req, res) => {
+// este si sirve 
+// router.get("/historico", validarApiKey, async (req, res) => {
+//     try {
+//         const historico = await Telemetria.find().sort({ timestamp: -1 }).limit(100);
+//         res.json(historico);
+//     } catch (error) {
+//         console.error('Error obteniendo histórico:', error);
+//         res.status(500).json({ error: "Error obteniendo histórico" });
+//     }
+// });
+router.get("/historico", async (req, res) => {
     try {
         const historico = await Telemetria.find().sort({ timestamp: -1 }).limit(100);
         res.json(historico);
