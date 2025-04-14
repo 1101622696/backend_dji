76557f90afbb6a2403aff357905570f62f627c10
unknown <diegomanuelcaca@gmail.com>
2025-04-14 11:17:17 -0500

    Eliminar definitivamente archivo de credenciales4

--- routes/solicitudes.js
+++ routes/solicitudes.js
@@ -4,9 +4,9 @@ import {validarJWT} from '../middlewares/validar-jwt.js'
 
 const router=Router()
 
-router.get("/",[validarJWT],httpSolicitudes.obtenerSolicitudes)
-
-router.post("/crear",[validarJWT],httpSolicitudes.crearSolicitud)
-
+// router.get("/",[validarJWT],httpSolicitudes.obtenerSolicitudes)
+// router.post("/crear",[validarJWT],httpSolicitudes.crearSolicitud)
+router.get("/",httpSolicitudes.obtenerSolicitudes)
+router.post("/crear",httpSolicitudes.crearSolicitud)
 
 export default router
