095bb5ffb1c89cb61ca6fba6bbf59445c72fe641
unknown <diegomanuelcaca@gmail.com>
2025-04-21 11:38:03 -0500

    prueba23

--- controllers/solicitudes.js
+++ controllers/solicitudes.js
@@ -6,14 +6,14 @@ crearSolicitud: async (req, res) => {
   try {
     console.log("Usuario token:", req.usuariobdtoken);
     
-    if (!req.usuariobdtoken) {
-      return res.status(401).json({ mensaje: 'No autorizado: información de usuario no encontrada' });
-    }
+    // if (!req.usuariobdtoken) {
+    //   return res.status(401).json({ mensaje: 'No autorizado: información de usuario no encontrada' });
+    // }
     
     // Usar las propiedades correctas según tu estructura
-    const useremail = req.usuariobdtoken.email;
-    const username = req.usuariobdtoken.nombre;
-    // const { email, nombre } = req.usuariobdtoken;
+    // const useremail = req.usuariobdtoken.email;
+    // const username = req.usuariobdtoken.nombre;
+    const { email, nombre } = req.usuariobdtoken;
 
     const {proposito, fecha_inicio, hora_inicio, fecha_fin, hora_fin, empresa, peso_maximo, detalles_cronograma, departamento, municipio, tipodecontactovisualconlaua, vueloespecial, justificacionvueloespecial, poligononombre, altura_poligono, latitud_poligono_1, longitud_poligono_1, latitud_poligono_2, longitud_poligono_2, latitud_poligono_3, longitud_poligono_3, latitud_poligono_4, longitud_poligono_4, latitud_poligono_5, longitud_poligono_5, tramolinealnombre, altura_tramo, latitud_tramo_1, longitud_tramo_1, latitud_tramo_2, longitud_tramo_2, latitud_tramo_3, longitud_tramo_3, latitud_tramo_4, longitud_tramo_4, latitud_tramo_5, longitud_tramo_5, circuferenciaencoordenadayradionombre, altura_circunferencia, latitud_circunferencia_1, longitud_circunferencia_1, check_kmz, realizado  } = req.body;
 
