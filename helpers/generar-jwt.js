e7ddf1f00bcdc31dca9e2fb02d45ba5b15e3c136
unknown <diegomanuelcaca@gmail.com>
2025-04-21 11:33:53 -0500

    prueba22

--- helpers/generar-jwt.js
+++ helpers/generar-jwt.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generarJWT = (id, perfil) => {
   return new Promise((resolve, reject) => {
-    const payload = { id, perfil };
+    const payload = { id, perfil, email, nombre };
     jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
       expiresIn: '4h',
     }, (err, token) => {
