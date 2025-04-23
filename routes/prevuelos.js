61a4a81b005e85b6cae63a362cee931a5078ba6b
unknown <diegomanuelcaca@gmail.com>
2025-04-23 14:45:18 -0500

    prueba34

--- routes/prevuelos.js
+++ routes/prevuelos.js
@@ -7,12 +7,19 @@ const router=Router()
 // router.get("/",[validarJWT],httpPrevuelos.obtenerprevuelos)
 
 router.get("/",[validarJWT],httpPrevuelos.obtenerprevuelos)
+router.get("/pcantidad",[validarJWT],httpPrevuelos.obtenerprevuelosCantidad)
 router.get("/pendientes",[validarJWT],httpPrevuelos.obtenerPrevuelosPendientes)
+router.get("/pendientes/cantidad",[validarJWT],httpPrevuelos.obtenerPrevuelosPendientesCantidad)
 router.get("/aprobados",[validarJWT],httpPrevuelos.obtenerPrevuelosAprobadas)
+router.get("/aprobados/cantidad",[validarJWT],httpPrevuelos.obtenerPrevuelosAprobadasCantidad)
 router.get("/enproceso",[validarJWT],httpPrevuelos.obtenerPrevuelosEnProceso)
+router.get("/enproceso/cantidad",[validarJWT],httpPrevuelos.obtenerPrevuelosEnProcesoCantidad)
 router.get("/pendientes/email/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosPendientesPorEmail)
+router.get("/pendientes/email/cantidad/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosPendientesPorEmailCantidad)
 router.get("/aprobados/email/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosAprobadosPorEmail)
+router.get("/aprobados/email/cantidad/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosAprobadosPorEmailCantidad)
 router.get("/enproceso/email/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosEnProcesoPorEmail)
+router.get("/enproceso/email/cantidad/:email",[validarJWT],httpPrevuelos.obtenerPrevuelosEnProcesoPorEmailCantidad)
 router.get('/email/:email',[validarJWT], httpPrevuelos.obtenerPrevuelosPorEmail);
 router.get('/filtrar-completo',[validarJWT], httpPrevuelos.obtenerPrevuelosPorEmailYEstado);
 router.get('/obtenerdatosprevuelo/:consecutivo',[validarJWT], httpPrevuelos.obtenerPrevueloPorConsecutivo);
